554b865c0c3b9414b1ae2e4be0d492e4ee0e3a5d
kapeiharshad <kapeiharshad@gmail.com>
2021-01-19 13:42:12 +0530

    capitalize done

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,12 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let words = [];
+  for (let [i, v] of str.split(" ").entries()) {
+    words.push(v[0].toUpperCase() + v.slice(1));
+  }
+  return words.join(" ");
+}
 
 module.exports = capitalize;
