ec4571c9b5fdf3ac6f8e1355df6e004c063f522a
kapeiharshad <kapeiharshad@gmail.com>
2021-01-19 12:21:49 +0530

    chunk and anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,36 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function anagrams(stringA, stringB) {
+//   let charMapA = buildCharMap(stringA);
+//   let charMapB = buildCharMap(stringB);
+
+//   if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//     return false;
+//   }
+//   for (const i in charMapA) {
+//     if (charMapA[i] !== charMapB[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+// function buildCharMap(str) {
+//   let obj = {};
+//   for (const i of str.replace(/[^\w]/g, "").toLowerCase()) {
+//     obj[i] = obj[i] ? obj[i] + 1 : 1;
+//   }
+//   return obj;
+// }
+
+function anagrams(stringA, stringB) {
+  if (buildCharMap(stringA) === buildCharMap(stringB)) {
+    return true;
+  }
+  return false;
+}
+function buildCharMap(str) {
+  return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join();
+}
 
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,21 +9,26 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
+  console.log("from chunk:::");
   // let arr = [];
   // let start = 0;
   // let end = size;
   // while (start < array.length) {
-  //   arr.push(array.slice(start, end) );
+  //   arr.push(array.slice(start, end));
   //   start = end;
   //   end += size;
   // }
   // return arr;
 
-
-  const 
-
-  let arr=[];
-  
+  let arr = [];
+  for (let element of array) {
+    let last = arr[arr.length - 1];
+    if (!last || last.length == size) {
+      arr.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
 }
 
 module.exports = chunk;
