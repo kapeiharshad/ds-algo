7b073c3956595e82d460dee37bc3d943c7d69cfb
kapeiharshad <kapeiharshad@gmail.com>
2021-01-21 15:38:52 +0530

    vowel

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,26 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  //   const vowel = ["a", "e", "i", "o", "u"];
+  //   let count = 0;
+  //   for (const i of str) {
+  //     for (const j of vowel) {
+  //       if (i.toLowerCase() === j) {
+  //         count++;
+  //       }
+  //     }
+  //   }
+
+  const vowel = ["a", "e", "i", "o", "u"];
+  let count = 0;
+  for (const i of str) {
+    let check = vowel.includes(i.toLowerCase());
+    if (check) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = vowels;
