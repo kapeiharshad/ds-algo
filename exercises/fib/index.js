ca0a2c4de4fbcfa1febe2bc54d8ae0c24c9ab82b
kapeiharshad <kapeiharshad@gmail.com>
2021-01-27 13:17:31 +0530

    fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,45 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//   let num1 = 0;
+//   let num2 = 1;
+//   let fib;
+//   if (n === 1) {
+//     return 1;
+//   }
+//   for (let i = 0; i < n - 1; i++) {
+//     fib = num1 + num2;
+//     num1 = num2;
+//     num2 = fib;
+//   }
+//   return fib;
+// }
+
+// Recursion
+
+//  this is a memoization function
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowfib(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowfib);
 
 module.exports = fib;
