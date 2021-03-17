fb3c21ce30637e7e24564b8313a9921edf83f547
Harshad Kapei <kapeiharshad@gmail.com>
2021-03-17 13:48:27 +0530

    removedAt, inserAt , getAt added

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -73,17 +73,50 @@ class LinkedList {
     }
   }
   getAt(index) {
+    let counter = 0;
     let node = this.head;
-    if (!node) {
-      return null
-    }
-    for (let i = 0; i <= index; i++) {
-      if (i===index) {
-        return node
+    while (node) {
+      if (counter === index) {
+        return node;
       }
-      node = node.next
+
+      counter++;
+      node = node.next;
     }
-    return null
+    return null;
+  }
+  removeAt(index){
+    if (!this.head) {
+      return
+    }
+    if (index===0) {
+      this.head = this.head.next
+      return
+    }
+
+    let pre = this.getAt(index-1)
+    if (!pre|| !pre.next) {
+      return
+    }
+    pre.next=pre.next.next;
+  }
+  insertAt(data,index){
+    if (index===0) {
+      const node=new Node(data)
+      node.next=this.head
+      this.head=node
+      return
+    }
+    let pre = this.getAt(index-1);
+    let node = this.getAt(index)
+    if (pre || node) {
+      pre.next=new Node(data);
+      pre.next.next=node
+    } else {
+      const node=new Node(data)
+      this.getLast().next=node
+    }
+
   }
 }
 module.exports = { Node, LinkedList };
